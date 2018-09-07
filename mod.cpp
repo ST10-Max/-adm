dcd760bc50118c8a506731a22807421e1869c9d5
ST10-Max <bluefire31995@yahoo.de>
2018-09-07 14:08:21 +0200

    Add files via upload

---
+++ mod.cpp
@@ -0,0 +1,13 @@
+name = "Advanced Diving Module v0.2";
+picture = "hintergrund.paa"; 
+tooltipOwned = "Version 0.2 Buhlmann";
+dlcColor[] = 
+{
+	0.0,
+	0.0,
+	1,
+	1
+};
+overview = "A mod that simulates diving as close as possible to the real world. Contains a variety of breathing gases, diving equipment and a diving computer. More features will be added over time.";
+hideName = 0;
+hidePicture = 0;
